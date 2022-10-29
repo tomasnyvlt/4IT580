9ff0a011720c6f93767ebeb9cec585e5187b2104
Tomáš Nývlt <tomasnyvlt@appio.cz>
2022-10-29 21:39:48 +0200

    Add useMutation hook

--- frontend/components/layouts/Landing.tsx
+++ frontend/components/layouts/Landing.tsx
@@ -1,5 +1,7 @@
 import { Box } from "@chakra-ui/react";
-import { FC, PropsWithChildren } from "react";
+import { UserContext } from "components/contexts/UserContext";
+import { useRouter } from "next/router";
+import { FC, PropsWithChildren, useContext, useEffect } from "react";
 
 import Footer from "components/ui/Footer";
 import Header from "components/ui/Header";

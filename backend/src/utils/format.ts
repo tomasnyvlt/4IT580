f63a90da534014f309e031d305bb8f88ac2f48e8
Miško Dvořák <michal.dvorak999@gmail.com>
2022-11-02 18:51:59 +0100

    Added registration validation.

--- backend/src/utils/format.ts
+++ backend/src/utils/format.ts
@@ -2,7 +2,14 @@ import { user } from "@prisma/client";
 
 export const formatPlayers = (players: Array<user>) => {
   return players.map((result) => {
-    return formatPlayer(result);
+    return {
+      id: result.id_user,
+      firstName: result.first_name,
+      lastName: result.last_name,
+      email: result.email,
+      timeRegistered: result.time_registered?.toISOString(),
+      timeLastLogin: result.time_last_login?.toISOString(),
+    };
   });
 };
 

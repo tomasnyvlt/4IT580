ddd20dd6cde576f7fee0900ef89f40419e71d94c
Tomáš Nývlt <tomasnyvlt@appio.cz>
2022-11-02 17:48:12 +0100

    Change apiUrl

--- frontend/config.ts
+++ frontend/config.ts
@@ -2,5 +2,5 @@ export const AUTH_TOKEN = "auth-token";
 export const REFRESH_TOKEN = "refresh-token";
 
 export default {
-  apiUrl: "http://dev.backend.team05.vse.handson.pro/graphql"
+  apiUrl: "https://dev-backend-team05-vse.handson.pro/graphql"
 };

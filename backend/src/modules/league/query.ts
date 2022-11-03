5eab66dc7062be4b25e196920cde15e94d4a381d
Štěpán Novotný <stepan.novotny@protonmail.com>
2022-11-03 20:52:41 +0100

    BE: League admin and league teams resolvers

--- backend/src/modules/league/query.ts
+++ backend/src/modules/league/query.ts
@@ -1,9 +1,9 @@
-export const Leagues = async (_: void, __: void, context: Context) => {
+export const leagues = async (_: void, __: void, context: Context) => {
   const leagues = await context.prisma.league.findMany();
   return leagues.slice(0, 20);
 };
 
-export const League = async (
+export const league = async (
   _: void,
   args: { id_league: number },
   context: Context
--- backend/src/modules/league/schema.ts
+++ backend/src/modules/league/schema.ts
@@ -1,21 +1,23 @@
 import { gql } from "apollo-server-core";
 
 export const typeDef = /* GraphQL */ `
-    type League{
-        id_league: Int!
-        name: String!
-        description: String
-        image_url: String!
-    }
+  type League {
+    id_league: Int!
+    name: String!
+    description: String
+    image_url: String!
+    admin: User
+    teams: [Team]
+  }
 
-    extend type Query {
-        leagues: [League!]!
-        league(id_league: Int!): League!
-    }
+  extend type Query {
+    leagues: [League!]!
+    league(id_league: Int!): League!
+  }
 
-    extend type Mutation{
-        addLeague(name: String!, description: String, image_url: String!): League!
-        deleteLeague(id_league: Int!): String!
-        updateLeague(id_league: Int!): String!
-    }
+  extend type Mutation {
+    addLeague(name: String!, description: String, image_url: String!): League!
+    deleteLeague(id_league: Int!): String!
+    updateLeague(id_league: Int!): String!
+  }
 `;

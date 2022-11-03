b46c30869e1290bb4afee3299b64fa7951b1b802
Štěpán Novotný <stepan.novotny@protonmail.com>
2022-11-03 20:52:41 +0100

    BE: League admin and league teams resolvers

--- backend/src/modules/league/index.ts
+++ backend/src/modules/league/index.ts
@@ -1,10 +1,16 @@
-import { typeDef } from './schema.js';
-import { IResolvers } from "@graphql-tools/utils"
-import * as mutations from './mutation.js';
+import { typeDef } from "./schema.js";
+import { IResolvers } from "@graphql-tools/utils";
+import * as queries from "./query.js";
+import * as mutations from "./mutation.js";
+import * as leagueSpecific from "./league_specific.js";
 
 const resolvers: IResolvers<any, Context> = {
-    Query: { },
-    Mutation: { ...mutations },
-  };
-  
-  export { typeDef, resolvers };
+  Query: { ...queries },
+  Mutation: { ...mutations },
+  League: {
+    admin: leagueSpecific.leagueAdmin,
+    teams: leagueSpecific.leagueTeams,
+  },
+};
+
+export { typeDef, resolvers };
--- backend/src/utils/format.ts
+++ backend/src/utils/format.ts
@@ -1,15 +1,19 @@
-import { user } from '@prisma/client';
+import { user } from "@prisma/client";
 
 export const formatPlayers = (players: Array<user>) => {
   return players.map((result) => {
-    return {
-      id: result.id_user,
-      firstName: result.first_name,
-      lastName: result.last_name,
-      userName: result.user_name,
-      email: result.email,
-      timeRegistered: result.time_registered?.toISOString(),
-      timeLastLogin: result.time_last_login?.toISOString(),
-    };
+    return formatPlayer(result);
   });
+};
+
+export const formatPlayer = (player: user) => {
+  return {
+    id: player.id_user,
+    firstName: player.first_name,
+    lastName: player.last_name,
+    userName: player.user_name,
+    email: player.email,
+    timeRegistered: player.time_registered?.toISOString(),
+    timeLastLogin: player.time_last_login?.toISOString(),
+  };
 };
